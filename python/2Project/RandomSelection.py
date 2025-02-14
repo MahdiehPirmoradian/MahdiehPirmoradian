63a0675652fe677c2c29bb9659eec8148ac840b5
Mahdieh Pirmoradian <mahdieh.pirmoradian@stud.fra-uas.de>
2025-02-14 21:22:46 +0100

    My Changes

---
+++ python/2Project/RandomSelection.py
@@ -0,0 +1,7 @@
+from random import choice
+# Create a list of foods in teh menue
+
+Resturant_Menu = ['Burger', 'spaghetti', 'Pommes', 'Salad']
+
+#print a random food from animes
+print(choice(Resturant_Menu))
